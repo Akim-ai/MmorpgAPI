926c9984b7bd7922ca71ee92b58c142d4c5092dc
Akim-ai <78012844+Akim-ai@users.noreply.github.com>
2021-10-30 02:58:04 +0300

    deleting, managers
    Fake deleting for models: Announcement, AnnouncementPicture, AnnouncementResponse, Avatar(oauth), Follower(oauth).
    managers:
    1) DefaultManager: Only not deleted objects
    Access- instance.objects.method()
    2) DeletedManager: Only deleted objects.
    Access- instance.objects_del.method()
    3) Manager: is the default manager to get all the objects.
    Access- instance.objects_all.method()

--- HelpCode/models/managers.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class DefaultManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(deleted=False).order_by('create_date')
